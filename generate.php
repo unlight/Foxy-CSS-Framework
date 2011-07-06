b35a448208d7dc610b38049a982435da209f1f65
S <unlights@gmail.com>
2011-07-06 12:39:54 +0400

    Playing

--- generate.php
+++ generate.php
@@ -9,9 +9,11 @@ $CssRules = array();
 $WidthRuleFormat['A'] = 'calc(100%%/%d - 10px);';
 $WidthRuleFormat['Z'] = 'calc(100%% - 100%%/%d - 10px);';
 
-for ($i = 1; $i <= 12; $i++) {
+for ($i = 1; $i <= 6; $i++) {
 	foreach (array('A', 'Z') as $Name) {
 		$Class = '.' . $Name . $i;
+		//if ($Name == 'A') $CssRules[$Class][] = "width: " . (100 / $i - 1.76) . '%;';
+		//if ($Name == 'Z') $CssRules[$Class][] = "width: " . (100 - 100/$i - 1.76) . '%;';
 		$Format = $WidthRuleFormat[$Name];
 		foreach ($VendorPrefixes as $Px) {
 			$CssRules[$Class][] = 'width: ' . $Px . sprintf($Format, $i);
